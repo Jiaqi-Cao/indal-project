4e14ea91fb671dacf6651e476c835250f0c5257a
Jiaqi Cao <mejakiew@gmail.com>
2019-03-08 17:30:10 -0400

    Jiaqi Cao, Sherry Kuang Wrote the UI for the history courses and an empty activity

---
+++ indal-new_interface 2/app/src/main/java/ca/indal/app/android/HistoryCourse.java
@@ -0,0 +1,15 @@
+package ca.indal.app.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HistoryCourse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_course);
+
+
+    }
+}
