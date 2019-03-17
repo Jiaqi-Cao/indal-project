660cc4c698b651e75e8e7d3e3f3d8919151ae8b5
yujiao <yj697879@dal.ca>
2019-03-17 14:01:22 -0300

    Jessie Wang & Yang Shu write feedback class and feedback test

---
+++ app/src/test/java/ca/indal/app/android/FeedbackTest.java
@@ -0,0 +1,30 @@
+package ca.indal.app.android;
+
+
+
+import org.junit.Test;
+
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static org.junit.Assert.*;
+
+
+public class FeedbackTest{
+
+
+    private static String urltest = "http://news.app.indal.ca/feedback/";
+
+
+
+
+    @Test
+    public void urltest() throws IOException{
+        URL url = new URL(urltest);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        int status = connection.getResponseCode();
+        assertEquals(200,status);
+    }
+}
